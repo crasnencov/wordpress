9a6aeb040b6c7e32c7c34d5fc80a7c427a49f07e
crasnencov <crasnencov@gmail.com>
2013-07-01 15:12:26 +0300

    face

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * HUI to the WordPress application. This file doesn't do anything, but loads
+ * BASSS to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
  *
  * @package WordPress
